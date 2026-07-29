b9f81d5e5f8ea3500d293c6c152ce82c4e50a640
Emilio Zambrano <emiliozambranovite@gmail.com>
2026-07-29 09:10:13 -0500

    ModeloEspecialistas

---
+++ models/Especialista.js
@@ -0,0 +1,26 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../db/conexion.js';
+
+export const Especialidad = sequelize.define(
+    'Especialidad',
+    {
+        id_especialidad: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        nombre: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            unique: true,
+        },
+        descripcion: {
+            type: DataTypes.STRING(255),
+            allowNull: true,
+        },
+    },
+    {
+        tableName: 'especialidades',
+        timestamps: true,
+    }
+);
